75f04b29867f1e69ebfad4c156ec835d11314737
Boemia <gabrielresende091002@gmail.com>
2024-05-06 17:24:06 -0300

    Operadores

---
+++ TiposVariaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args){
+
+        double adicao = 3.4 + 2.1;
+        int subtracao = 3 - 1;
+        int multiplicacao = 3 * 5;
+        int diviso = 25 / 5;
+        int modulo = 18 % 3;
+        double resultado = (10*3) + (16+4);
+
+        String nomeCompleto = "Linguagem" + " Java";
+        System.out.println(nomeCompleto);
+
+    }
+}
--- TiposVariaveis/src/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numeroInteiro = 12;
+        byte numeracao = 123;
+        double salarioMinimo = 1600;
+        short numeroCurto = 43;
+        double salario = 1690.33;
+        boolean doador = true;
+
+        //Prcisa do L.
+        long cpf = 423456782123L;
+
+        //Precisa do F.
+        float pi = 3.14F;
+
+        float teste = 3.24F;
+        int teste2 = (int) teste;
+
+        //variavel / constante.
+
+        int variavel = 2;
+
+        final int CONSTANTE = 2;
     }
 }
